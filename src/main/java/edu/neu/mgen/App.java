1619de4fcab6321c70c70bf151062648059881ca
Zichuan Fan <zichuanfan1108@gmail.com>
2026-09-21 22:20:12 -0700

    Initial commit

---
+++ src/main/java/edu/neu/mgen/App.java
@@ -0,0 +1,18 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /*
+        * This program prints a message.
+        * It is my first Java program
+        */
+        // Print a message to the console
+        System.out.println( "It is my first Java program" );
+    }
+}
